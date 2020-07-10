6ba8a42f85caf06f4d843d0dc80ad04b51c5bbff
Alex <format37@gmail.com>
2020-07-10 15:56:55 +0300

    new file:   server.py

---
+++ server.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+import asyncio
+from aiohttp import web
+import urllib
+import urllib.parse
+from urllib.parse import urlparse, parse_qsl
+import multidict as MultiDict
+import requests
+from map_gen import map_generator
+
+async def addr_to_geo(request):
+	addr	= request.rel_url.query['addr']
+	content = addr	
+	return web.Response(text=content,content_type="text/html")
+
+async def call_check(request):	
+	content = "ok"	
+	return web.Response(text=content,content_type="text/html")
+
+
+app = web.Application()
+app.router.add_route('GET', '/addr_to_geo', call_addr_to_geo)
+app.router.add_route('GET', '/check', call_check)
+
+loop = asyncio.get_event_loop()
+handler = app.make_handler()
+f = loop.create_server(handler, port='8081')
+srv = loop.run_until_complete(f)
+
+print('serving on', srv.sockets[0].getsockname())
+try:
+	loop.run_forever()
+except KeyboardInterrupt:
+	print("serving off...")
+finally:
+	loop.run_until_complete(handler.finish_connections(1.0))
+	srv.close()
